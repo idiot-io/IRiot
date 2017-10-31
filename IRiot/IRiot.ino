2629995fbf2593ca4092f4e14e9d5c283adb5ee9
5shekel <yair99@gmail.com>
2017-10-31 19:34:25 +0200

    touchlib intgration

--- IRiot/IRiot.ino
+++ IRiot/IRiot.ino
@@ -1,46 +1,18 @@
-//more info at https://github.com/shenkarSElab/IRiot
+#include <avr/io.h>
+#include <util/delay.h>
+#include "TinyTouchLib.h"
 
-/*
- * "With timer1 running off the PLL @ 64mhz, tone() should be able to output a 32mhz signal on pin 4!"
- */
- 
-#include "tiny_IRremote.h"
+int main(void)
+{
+  CLKPR=_BV(CLKPCE);
+  CLKPR=0;      // set clock prescaler to 1 (attiny 25/45/85/24/44/84/13/13A)   
 
-#define ledPin    0     // pin5 / PB0
-//#define touchPin  
-IRsend irsend;          // pin3 / PB4 / Digital 4 / TIMER1 output compare unit
-
-//change 00000 to a NEC code!
-#define YOUR_NEC_CODE 9000
-
-int ledState = LOW;
-unsigned long previousMillis = 0;
-const long interval = 1000;
-
-void setup() {
-
-  pinMode(ledPin, OUTPUT);
-  digitalWrite(ledPin, LOW);
-}
-
-void loop() {
-  unsigned long currentMillis = millis();
-
-  if (currentMillis - previousMillis >= interval) {
-    previousMillis = currentMillis;
-
-    if (ledState == LOW) {
-      ledState = HIGH;
-      send_ir();
-    } else {
-      ledState = LOW;
-    }
-
-    digitalWrite(ledPin, ledState);
-
-  }
-}
-
-void send_ir() {
-  irsend.sendNEC(YOUR_NEC_CODE, 32); // sending the nec code
+  DDRB=_BV(PB4);    // Enable LED output pin
+  
+  tinytouch_init();
+  
+  while(1) {
+    if (tinytouch_sense()==tt_push) {PORTB^=_BV(PB4);}  // Toggle LED on touch event    
+    _delay_ms(10);  
+  } 
 }
