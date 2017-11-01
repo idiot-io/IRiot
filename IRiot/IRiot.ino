6fb245d25c501534fa0a8aa9482df0abbc219126
5shekel <yair99@gmail.com>
2017-11-01 08:53:16 +0200

    nice gfx

--- IRiot/IRiot.ino
+++ IRiot/IRiot.ino
@@ -1,22 +1,22 @@
-//more info at https://github.com/shenkarSElab/IRiot
-
 /*
-   "With timer1 running off the PLL @ 64mhz, tone() should be able to output a 32mhz signal on pin 4!"
-*/
-
+ * words http://idiot.io
+ * code https://github.com/shenkarSElab/IRiot
+ * 
+ * pin out attiny85
+ * NC      --+----+-- VCC
+ * Touch   --|*   |-- NC 
+ * IRsend  --|    |-- NC
+ * GND     --+----+-- LED
+ * 
+  */
+/
 #include "tiny_IRremote.h"
 
-
 #define ledPin    0     // pin5 / PB0
-//#define touchPin
 IRsend irsend;          // pin3 / PB4 / Digital 4 / TIMER1 output compare unit
-
-//change 00000 to a NEC code!
 #define YOUR_NEC_CODE 9000
 
 int ledState = LOW;
-unsigned long previousMillis = 0;
-const long interval = 1000;
 
 /*
    ======================================================
@@ -68,8 +68,7 @@ void setup() {
 }
 
 void loop() {
-  //unsigned long currentMillis = millis();
-
+  
   uint8_t touchstate = tinytouch_sense();
   /*
     // Possible return values are:
@@ -82,29 +81,11 @@ void loop() {
 
   if (touchstate == tt_push) {
     ledState = HIGH;
-    send_ir();
+    irsend.sendNEC(YOUR_NEC_CODE, 32); // sending the nec code
   } else {
     ledState = LOW;
   }
-
-  /*
-     //non blocking blink test
-    if (currentMillis - previousMillis >= interval) {
-      previousMillis = currentMillis;
-
-      if (ledState == LOW) {
-        ledState = HIGH;
-        send_ir();
-      } else {
-        ledState = LOW;
-      }
-    }
-  */
   digitalWrite(ledPin, ledState);
-}
-
-void send_ir() {
-  irsend.sendNEC(YOUR_NEC_CODE, 32); // sending the nec code
 }
 
 
