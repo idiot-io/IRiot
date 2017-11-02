83077347a6958834a6ce5b96b6c1614b16b7a8ef
yair <yair99@gmail.com>
2017-11-02 22:26:59 +0200

    added push pull states

--- IRiot/IRiot.ino
+++ IRiot/IRiot.ino
@@ -1,15 +1,15 @@
 /*
- * words http://idiot.io
- * code https://github.com/shenkarSElab/IRiot
- * 
- * pin out attiny85
- * NC      --+----+-- VCC
- * Touch   --|*   |-- NC 
- * IRsend  --|    |-- NC
- * GND     --+----+-- LED
- * 
-  */
-/
+   words http://idiot.io
+   code https://github.com/shenkarSElab/IRiot
+
+   pin out attiny85
+   NC      --+----+-- VCC
+   Touch   --|*   |-- NC
+   IRsend  --|    |-- NC
+   GND     --+----+-- LED
+
+*/
+
 #include "tiny_IRremote.h"
 
 #define ledPin    0     // pin5 / PB0
@@ -49,6 +49,19 @@ int ledState = LOW;
 #define tt_sensepin 3 // Use PB3 as sense pin
 #define tt_senseadc 3 // Use ADC3 as sense ADC input
 
+// The sense function evaluates the button state and performs internal
+// housekeeping. It should be polled at least 30 times per second to
+// update the internal logic. Please note that each call performs 32
+// analog to digital conversions with active waiting. This may take
+// several ms.
+
+// Possible return values are:
+//    tt_off=0  No touch sensed
+//    tt_on   Touch button is active and touch is sensed.
+//    tt_push   Touch button is pushed. Use this to initiate one time events.
+//    tt_release  Touch button is released. Use this to initiate one time events.
+//    tt_timeout  Touch button has been active too long and internal bias was reset.
+
 enum {tt_off = 0, tt_on, tt_push, tt_release, tt_timeout};
 // Internal function to read the adc input
 uint8_t tinytouch_adc(void);
@@ -68,24 +81,18 @@ void setup() {
 }
 
 void loop() {
-  
-  uint8_t touchstate = tinytouch_sense();
-  /*
-    // Possible return values are:
-    //   tt_off=0  No touch sensed
-    //    tt_on   Touch button is active and touch is sensed.
-    //    tt_push   Touch button is pushed. Use this to initiate one time events.
-    //    tt_release  Touch button is released. Use this to initiate one time events.
-    //    tt_timeout  Touch button has been active too long and internal bias was reset.
-  */
 
+  uint8_t touchstate = tinytouch_sense();
   if (touchstate == tt_push) {
     ledState = HIGH;
     irsend.sendNEC(YOUR_NEC_CODE, 32); // sending the nec code
-  } else {
-    ledState = LOW;
+    digitalWrite(ledPin, ledState);
   }
-  digitalWrite(ledPin, ledState);
+  if (touchstate == tt_release) {
+    ledState = LOW;
+    digitalWrite(ledPin, ledState);
+  }
+  
 }
 
 
