987499cc2f671ec464ee4c5bcc1f5ab0861bf1da
5shekel <yair99@gmail.com>
2017-12-23 10:55:48 +0200

    works on attiny85

--- thermoCandy/thermoCandy.ino
+++ thermoCandy/thermoCandy.ino
@@ -1,5 +1,23 @@
-//thermoCandy
-//for kids only
+
+/*
+  ThermoCandy
+   for kids only
+
+   words http://idiot.io
+   code https://github.com/shenkarSElab/IRiot/thermoCandy
+
+   pin out attiny85
+   NC      -1+----+8- VCC
+   Pixel   -2|*   |7- NC
+   Thermo  -3|    |6- NC
+   GND     -4+----+5- NC
+   http://drazzy.com/e/img/PinoutT85a.jpg
+*/
+
+///////////////////////////////////////////////
+///https://forum.arduino.cc/index.php?topic=112013.msg841582#msg841582
+#include "SendOnlySoftwareSerial.h"
+SendOnlySoftwareSerial mySerial (0);  // Tx pin , PB0, pin5
 
 ////////////////////////////////////////////////
 //https://github.com/adafruit/Adafruit_NeoPixel
@@ -7,58 +25,85 @@
 #ifdef __AVR__
 #include <avr/power.h>
 #endif
+#define pixelPin 3 //PB4, pin3
+Adafruit_NeoPixel pixels = Adafruit_NeoPixel(1, pixelPin, NEO_GRB + NEO_KHZ800);
 
-#define pixelPin 4
-Adafruit_NeoPixel pixels = Adafruit_NeoPixel(1, 4, NEO_GRB + NEO_KHZ800);
-//////////////////////////////////
-
-////////////////////////////////////
-//https://github.com/belidzs/PreciseLM35
-#include <PreciseLM35.h>
-const int pinLM35 = A0;
-PreciseLM35 lm35(pinLM35, DEFAULT);
-////////////////////////////////////
-
-/////////////////////////////////
-const int numReadings = 10;
-int readings[numReadings];      // the readings from the analog input
-int readIndex = 0;              // the index of the current reading
-int total = 0;                  // the running total
-int average = 0;                // the average
-
+///////////////////////////////
+// https://github.com/MajenkoLibraries/Average
+#include "Average.h"
+Average<float> ave(30);
+int sensorPin = A2; //pin 3
 
 void setup() {
-  Serial.begin(9600);
-  pixels.begin();
+  mySerial.begin(9600);
 
-  for (int thisReading = 0; thisReading < numReadings; thisReading++) {
-    readings[thisReading] = 0;
-  }
+  pixels.begin();
 }
 
 void loop() {
-  int temp = lm35.readCelsius();
-  
-  total = total - readings[readIndex];
-  readings[readIndex] = temp;
-  total += readings[readIndex];
-  readIndex += 1;
 
-  if (readIndex >= numReadings) {
-    readIndex = 0;
-  }
+  int analogValue = analogRead(sensorPin);
+  long temperature = (readVcc() / 1000.0) * analogValue * 100.0 / 1024;
 
-  average = total / numReadings;
-  Serial.println(String(average) + " " + String(temp));
+  ave.push(temperature);
+  float average = ave.mean();
 
 
   byte red, green, blue;
-  constrain(average, 24, 38);
-  red = map(average, 24, 32, 0, 255);
-  green = 255 - red;
-  blue = 0;
+  if (average > 24  && average < 35) {
+    green = mapfloat(average, 24, 37, 0, 255);
+    blue = 255 - red;
+    red = 0;
+  } else if (average >= 35  && average < 36.5) {
+    red = mapfloat(average, 24, 37, 0, 255);
+    green = 255 - red;
+    blue = 0;
+  } else if (average >= 36.5) {
+    red = mapfloat(average, 36.5, 37, 0, 255);
+    green = 0;
+    blue = 0;
+  }
 
   pixels.setPixelColor(0, pixels.Color(red, green, blue));
   pixels.show();
 
+  // And show some interesting results.
+  mySerial.print(ave.mean());
+  mySerial.print(" ");
+  mySerial.print(temperature);
+
+  mySerial.print(" ");
+  mySerial.print(red);
+  mySerial.print(" ");
+  mySerial.print(green);
+  mySerial.print(" ");
+  mySerial.println(blue);
+
+
+  delay(10);
+
+
 }
+
+
+// This code snippet is made by Tinkerit. Open Source license. Slightly modified by DavidJWatts
+long readVcc() {
+  // Read 1.1V reference against AVcc
+  long result;
+  // set the reference to Vcc and the measurement to the internal 1.1V reference
+  ADMUX = _BV(MUX3) | _BV(MUX2);
+
+  delay(2); // Wait for Vref to settle
+  ADCSRA |= _BV(ADSC); // Convert
+  while (bit_is_set(ADCSRA, ADSC));
+  result = ADCL;
+  result |= ADCH << 8;
+  result = 1126400L / result; // Back-calculate AVcc in mV
+  return result;
+}
+
+float mapfloat(long x, long in_min, long in_max, long out_min, long out_max)
+{
+  return (float)(x - in_min) * (out_max - out_min) / (float)(in_max - in_min) + out_min;
+}
+
